4b6b20c3091b2724077ee3785b55b1b2b678f867
Beatriz <beatriz.mediatto2@gmail.com>
2023-06-02 19:13:56 -0300

    feat: add study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    
-    raise NotImplementedError
+    if not len(permanence_period) or not target_time:
+        return None
+    count = 0
+
+    for time in permanence_period:
+        if type(time[0]) != int or type(time[1]) != int:
+            return None
+        if time[0] <= target_time <= time[1]:
+            count += 1
+
+    return count
